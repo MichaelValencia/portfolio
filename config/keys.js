3d0615a8ade5ead2a028932258bc82ab1a3a2d96
edgetech <mikevalencia5790@gmail.com>
2019-04-22 15:39:12 -0500

    upload new resume

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  MongoURI:
+    "mongodb://webroot:iSYSk7alJQCdDXnDgIA8nRaMc6d4GhZA9VV1gCe7CmrD57XRdajqm4B3y6A6X0FAeOdwLc7zEsBUROsHfHDMjA%3D%3D@webroot.documents.azure.com:10255/?ssl=true",
+  clientid:
+    "1038738770141-53dfn3plq0t1ank3dtc0u0ulmefnlmqm.apps.googleusercontent.com",
+  secret: "LLjjI8M4Ds4F4eugQM8sL30G",
+  googleadminid: "115961881797737181097",
+  baseURL: "http://localhost:3000/"
+};
